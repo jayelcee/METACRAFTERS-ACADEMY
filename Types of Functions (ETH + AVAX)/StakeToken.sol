e0e2238ce2b5bd997549622868b2af49b16b30f5
J <144415486+jayelcee@users.noreply.github.com>
2024-11-17 10:54:39 +0800

    added burn function

--- Types of Functions (ETH + AVAX)/StakeToken.sol
+++ Types of Functions (ETH + AVAX)/StakeToken.sol
@@ -5,11 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 // Functionality
 // Only contract owner should be able to mint tokens
 // Any user can transfer tokens
-// Any user can burn tokens
+// Any user can burn their own tokens
 
 contract StakeToken is ERC20, ERC20Burnable, Ownable {
     constructor(address initialOwner) 
@@ -17,12 +16,19 @@ contract StakeToken is ERC20, ERC20Burnable, Ownable {
         Ownable(initialOwner)
     {}
 
+    // Mint tokens - restricted to the contract owner
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-    
+
+    // Transfer tokens - allows token transfers
     function transfer(address _to, uint256 _value) public override returns (bool) {
         _transfer(msg.sender, _to, _value); 
         return true;
+    }
+
+    // Burn tokens - allows users to burn their own tokens
+    function burn(uint256 amount) public override {
+        _burn(msg.sender, amount);
     }
 }
